b7f7133e972cc7c650769839e3e1dd82dc365f08
Natalia Polishchuk <npolishchuk@grabitsystems.com>
2017-03-30 21:51:42 -0700

    create nested components within Avatar component

--- app/index.js
+++ app/index.js
@@ -1,15 +1,51 @@
+var USER_DATA = {
+  name: 'Natalia Polishchuk',
+  username: 'natapoli90',
+  image: 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAdTAAAAJGNmYmMxMDBjLTI1NWYtNDU0Zi1hYWRkLTc4ZjViNTQwOGJjMQ.jpg'
+};
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
+  render: function () {
+    return <img src={this.props.imageUrl} />
+  }
+});
+
+var ProfileLink = React.createClass({
   render: function () {
     return (
-      <div> Hello React! </div>
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
     )
   }
 });
 
+var ProfileName = React.createClass({
+  render: function () {
+    return (
+      <div>{this.props.name}</div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    )
+  }
+})
+
 ReactDOM.render(
-  <HelloWorld />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 );
